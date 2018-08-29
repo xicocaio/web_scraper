2c4a0b37765b2a98d6ca2440cbaf780e81705c53
Francisco Paiva <xicocaio@gmail.com>
2018-08-29 20:28:47 -0300

    Add querying google bq

--- app/__main__.py
+++ app/__main__.py
@@ -1,10 +1,8 @@
 from jobs.scrap import scrap_olx_iphone8
 from jobs.upload import load_to_gbq
-from jobs.query import get_avg_price_by_city, get_most_offers_by_city, get_most_least_expensive
+from jobs.query import run_all_queries
 
 if __name__ == "__main__":
     file_path = scrap_olx_iphone8()
     load_to_gbq(file_path)
-    get_avg_price_by_city()
-    get_most_offers_by_city()
-    get_most_least_expensive()
+    run_all_queries()
